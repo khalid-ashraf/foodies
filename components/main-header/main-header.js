629eb721814d2db7c1126b00d09d9da343d5ebb4
Syed Khalid <i.khalidashraf@gmail.com>
2024-05-21 20:54:26 -0400

    Styled mainheader

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,27 +1,32 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import MainHeaderBackground from "@/components/main-header/main-header-background/main-header-background";
 import classes from "./main-header.module.css";
 import logoImg from "@/assets/logo.png";
 
 export default function MainHeader() {
 	return (
-		<header className={classes.header}>
-			<Link href='/' className={classes.logo}>
-				<Image src={logoImg} alt='A plate with food on it' />
-				Foodies
-			</Link>
+		<>
+			<MainHeaderBackground />
 
-			<nav className={classes.nav}>
-				<ul>
-					<li>
-						<Link href='/meals'>Browse Meals</Link>
-					</li>
-					<li>
-						<Link href='/community'>Foodies Community</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
+			<header className={classes.header}>
+				<Link href='/' className={classes.logo}>
+					<Image src={logoImg} alt='A plate with food on it' priority />
+					Foodies
+				</Link>
+
+				<nav className={classes.nav}>
+					<ul>
+						<li>
+							<Link href='/meals'>Browse Meals</Link>
+						</li>
+						<li>
+							<Link href='/community'>Foodies Community</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</>
 	);
 }
