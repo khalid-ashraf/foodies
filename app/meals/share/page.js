905d81bb587a838f4159877ad568f4f066d2509f
Syed Khalid <i.khalidashraf@gmail.com>
2024-07-30 14:28:07 -0400

    created submit form server action

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,60 +1,60 @@
 import ImagePicker from "@/components/image-picker/image-picker";
 import classes from "./share-meal.module.css";
 
+import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit/meals-form-submit";
+
 export default function ShareMealPage() {
-	const shareMeal = async () => {
-		"use server";
-	};
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Share your <span className={classes.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
 
-	return (
-		<>
-			<header className={classes.header}>
-				<h1>
-					Share your <span className={classes.highlight}>favorite meal</span>
-				</h1>
-				<p>Or any other meal you feel needs sharing!</p>
-			</header>
+      <main className={classes.main}>
+        <form className={classes.form} action={shareMeal}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor='name'>Your name</label>
+              <input type='text' id='name' name='name' required />
+            </p>
 
-			<main className={classes.main}>
-				<form className={classes.form}>
-					<div className={classes.row}>
-						<p>
-							<label htmlFor='name'>Your name</label>
-							<input type='text' id='name' name='name' required />
-						</p>
+            <p>
+              <label htmlFor='email'>Your email</label>
+              <input type='email' id='email' name='email' required />
+            </p>
+          </div>
 
-						<p>
-							<label htmlFor='email'>Your email</label>
-							<input type='email' id='email' name='email' required />
-						</p>
-					</div>
+          <p>
+            <label htmlFor='title'>Title</label>
+            <input type='text' id='title' name='title' required />
+          </p>
 
-					<p>
-						<label htmlFor='title'>Title</label>
-						<input type='text' id='title' name='title' required />
-					</p>
+          <p>
+            <label htmlFor='summary'>Short Summary</label>
+            <input type='text' id='summary' name='summary' required />
+          </p>
 
-					<p>
-						<label htmlFor='summary'>Short Summary</label>
-						<input type='text' id='summary' name='summary' required />
-					</p>
+          <p>
+            <label htmlFor='instructions'>Instructions</label>
+            <textarea
+              id='instructions'
+              name='instructions'
+              rows='10'
+              required
+            />
+          </p>
 
-					<p>
-						<label htmlFor='instructions'>Instructions</label>
-						<textarea
-							id='instructions'
-							name='instructions'
-							rows='10'
-							required></textarea>
-					</p>
+          <ImagePicker label='Your Image' name='image' />
 
-					<ImagePicker />
-
-					<p className={classes.actions}>
-						<button type='submit'>Share Meal</button>
-					</p>
-				</form>
-			</main>
-		</>
-	);
+          <p className={classes.actions}>
+            <MealsFormSubmit />
+          </p>
+        </form>
+      </main>
+    </>
+  );
 }
--- components/image-picker/image-picker.js
+++ components/image-picker/image-picker.js
@@ -5,59 +5,59 @@ import classes from "./image-picker.module.css";
 import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
-	const [pickedImage, setPickedImage] = useState();
-	const imageInputRef = useRef();
+  const [pickedImage, setPickedImage] = useState();
+  const imageInputRef = useRef();
 
-	const handleImageChange = (e) => {
-		const file = e.target.files[0];
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
 
-		// If the user has selected no file, no need to proceed
-		if (!file) {
-			setPickedImage(null);
-			return;
-		}
+    // If the user has selected no file, no need to proceed
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
 
-		const fileReader = new FileReader();
+    const fileReader = new FileReader();
 
-		// This function will run once fileReader.readAsDataURL() is finished.
-		fileReader.onload = () => {
-			setPickedImage(fileReader.result);
-		};
+    // This function will run once fileReader.readAsDataURL() is finished.
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
 
-		fileReader.readAsDataURL(file);
-	};
+    fileReader.readAsDataURL(file);
+  };
 
-	return (
-		<div className={classes.picker}>
-			<label htmlFor={name}>{label}</label>
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
 
-			<div className={classes.controls}>
-				<div className={classes.preview}>
-					{!pickedImage ? (
-						<p>No image picked yet.</p>
-					) : (
-						<Image src={pickedImage} alt='image selected by user' fill />
-					)}
-				</div>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage ? (
+            <p>No image picked yet.</p>
+          ) : (
+            <Image src={pickedImage} alt='image selected by user' fill />
+          )}
+        </div>
 
-				<input
-					className={classes.input}
-					type='file'
-					id={name}
-					accept='image/png, image/jpeg'
-					name={name}
-					ref={imageInputRef}
-					onChange={handleImageChange}
-					required
-				/>
+        <input
+          className={classes.input}
+          type='file'
+          id={name}
+          accept='image/png, image/jpeg'
+          name={name}
+          ref={imageInputRef}
+          onChange={handleImageChange}
+          required
+        />
 
-				<button
-					className={classes.button}
-					type='button'
-					onClick={() => imageInputRef.current.click()}>
-					Pick an Image
-				</button>
-			</div>
-		</div>
-	);
+        <button
+          className={classes.button}
+          type='button'
+          onClick={() => imageInputRef.current.click()}>
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
 }
